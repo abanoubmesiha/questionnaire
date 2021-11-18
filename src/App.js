46fe702e9788cb42158cb78ca8ceefd4edcc7959
Abanoub Ayoub <abanoub@terkwaz.com>
2021-11-18 18:20:10 +0200

    Add bootstrap, reactstrap, and sass.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from 'reactstrap';
+import Header from './layout/header';
+import Footer from './layout/footer';
+import Home from './components/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.sass';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Container fluid>
+        <Home />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Hi</h1>
+    </section>
+  );
+};
+
+export default Home;
